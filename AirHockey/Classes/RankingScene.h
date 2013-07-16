0d8e5985c2905f463101cd44225bae00f5162a30
ngocdu <nguyen.ngoc.duh@framgia.com>
2013-07-16 15:54:20 +0700

    change scene begin game

--- AirHockey/Classes/RankingScene.h
+++ AirHockey/Classes/RankingScene.h
@@ -22,5 +22,6 @@ public:
     
     void menuPlay(CCObject* pSender);
     void menuMenu(CCObject* pSender);
+    void clickBtSendEmail(CCObject* pSender);
 };
 #endif /* defined(__AirHockey__RankingScene__) */
